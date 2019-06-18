ca480b175b8b867660740c0e1f06b4586c16fccc
Anya Snow <emailasnow@gmail.com>
2019-06-18 01:27:26 -0700

    basic setup

--- src/app.js
+++ src/app.js
@@ -4,31 +4,37 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN } = require('./config');
+
 
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+  ? 'tiny'
+  : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors())
+app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(helmet())
 
-app.get('/', (req, res) => {
-       res.send('Hello, world!')
-})
+app.get('/api/*', (req, res) => {
+  res.json({ ok: true });
+});
+
+
+
+
 
 app.use(function errorHandler(error, req, res, next) {
-      let response
-    if (NODE_ENV === 'production') {
-                response = { error: { message: 'server error' } }
-              } else {
-            console.error(error)
-            response = { message: error.message, error }
-          }
-      res.status(500).json(response)
-        })
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
 
 
 module.exports = app
