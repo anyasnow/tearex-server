2a924c6c9614e9f100635e44b28618bb809abf46
Anya Snow <emailasnow@gmail.com>
2019-06-22 01:26:34 -0700

    ready to deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8800,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://anya-snow@localhost/tearex',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://anya-snow@localhost/tearex',
+    // JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
 }
