6661643e05571d96f3e95df41eb7a9d9e9b046fd
Anya Snow <emailasnow@gmail.com>
2019-06-22 00:41:00 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8800,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     DB_URL: process.env.DB_URL || 'postgresql://anya-snow@localhost/tearex',
 }
